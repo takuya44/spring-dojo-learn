aab35c9171064c55a9c99d8906a1806fb8f5ccb4
takuya44 <shinntaku2345@gmail.com>
2024-06-10 23:29:09 +0900

    GET /articles/{id} を受けるコントローラーにJavaDocでコメントを追記

--- src/main/java/com/example/blog/web/controller/article/ArticleRestController.java
+++ src/main/java/com/example/blog/web/controller/article/ArticleRestController.java
@@ -4,10 +4,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * RESTコントローラークラス。このクラスは、記事に関するREST APIエンドポイントを提供します。
+ */
 @RestController
 public class ArticleRestController {
 
-  // Get /articles/1
+  /**
+   * 指定されたIDの記事を表示します。
+   *
+   * @param id 表示する記事のID
+   * @return 指定されたIDの記事を表す文字列
+   */
   @GetMapping("/articles/{id}")
   public String showArticle(@PathVariable("id") long id) {
     return "This is article: id = " + id;
