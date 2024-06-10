cc632ce895654b73d6a6376c9f3e4319ff8f3c05
takuya44 <shinntaku2345@gmail.com>
2024-06-11 00:07:54 +0900

    Controller のレスポンスを DTO にまとめる

--- src/main/java/com/example/blog/web/controller/article/ArticleRestController.java
+++ src/main/java/com/example/blog/web/controller/article/ArticleRestController.java
@@ -1,5 +1,6 @@
 package com.example.blog.web.controller.article;
 
+import java.time.LocalDateTime;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,15 +17,28 @@ public class ArticleRestController {
    * <p>具体例:
    * <pre>{@code
    * // URL: http://localhost:8080/articles/1
-   * // レスポンス: "This is article: id = 1"
+   * // レスポンス:
+   * // {
+   * //   "id": 1,
+   * //   "title": "This is title: id = 1",
+   * //   "content": "This is content",
+   * //   "createdAt": "2024-06-08T12:34:56.789",
+   * //   "updatedAt": "2024-06-08T12:34:56.789"
+   * // }
    * }</pre>
    * </p>
    *
    * @param id 表示する記事のID
-   * @return 指定されたIDの記事を表す文字列
+   * @return 指定されたIDの記事を表すArticleDTOオブジェクト
    */
   @GetMapping("/articles/{id}")
-  public String showArticle(@PathVariable("id") long id) {
-    return "This is article: id = " + id;
+  public ArticleDTO showArticle(@PathVariable("id") long id) {
+    return new ArticleDTO(
+        id,
+        "This is title: id = " + id,
+        "This is content",
+        LocalDateTime.now(),
+        LocalDateTime.now()
+    );
   }
 }
