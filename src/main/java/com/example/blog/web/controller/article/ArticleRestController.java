6ef9d4887bbe07b308970f5644d2333e0bf6998a
takuya44 <shinntaku2345@gmail.com>
2024-06-15 00:10:17 +0900

    Lombok を導入(コンストラクタが自動生成)

--- src/main/java/com/example/blog/web/controller/article/ArticleRestController.java
+++ src/main/java/com/example/blog/web/controller/article/ArticleRestController.java
@@ -1,6 +1,7 @@
 package com.example.blog.web.controller.article;
 
 import com.example.blog.service.article.ArticleService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,13 +12,10 @@ import org.springframework.web.server.ResponseStatusException;
  * RESTコントローラークラス。このクラスは、記事に関するREST APIエンドポイントを提供します。
  */
 @RestController
+@RequiredArgsConstructor
 public class ArticleRestController {
 
   private final ArticleService articleService;
-
-  public ArticleRestController(ArticleService articleService) {
-    this.articleService = articleService;
-  }
 
   /**
    * 指定されたIDの記事を表示します。
