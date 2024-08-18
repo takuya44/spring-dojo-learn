1d88b6fabfe0f304de9a9cb91aecab76ca092061
takuya44 <shinntaku2345@gmail.com>
2024-08-18 23:06:58 +0900

    add:自作 Filterから常に認証成功のタミーユーサーを返す

--- src/main/java/com/example/blog/web/filter/JsonUsernamePasswordAuthenticationFilter.java
+++ src/main/java/com/example/blog/web/filter/JsonUsernamePasswordAuthenticationFilter.java
@@ -2,6 +2,7 @@ package com.example.blog.web.filter;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -11,7 +12,10 @@ public class JsonUsernamePasswordAuthenticationFilter extends UsernamePasswordAu
   @Override
   public Authentication attemptAuthentication(HttpServletRequest request,
       HttpServletResponse response) throws AuthenticationException {
-    // 継承元のメソッドを呼び出すだけ（作り途中なので、ブレークポイントを貼るために）
-    return super.attemptAuthentication(request, response);
+    return UsernamePasswordAuthenticationToken.authenticated(
+        "dummy-user",     // 認証されたユーザー名
+        "dummy-password",         // 認証されたパスワード（今回はダミー値）
+        null                      // 認証されたユーザーの権限（今回はnull）
+    );
   }
 }
