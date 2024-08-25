c876b5dab5c37a7bad4fe3e60a9df8a5092074ae
takuya44 <shinntaku2345@gmail.com>
2024-08-25 19:38:33 +0900

    fix: UserDetailsServiceインターフェースを実装

--- src/main/java/com/example/blog/config/SecurityConfig.java
+++ src/main/java/com/example/blog/config/SecurityConfig.java
@@ -10,12 +10,9 @@ import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.authentication.session.ChangeSessionIdAuthenticationStrategy;
@@ -91,26 +88,6 @@ public class SecurityConfig {
     // セキュリティコンテキストをHTTPセッションに保存
     // ユーザーが一度認証されると、その認証情報がセッションを通じて保持され、認証済みの状態が維持される
     return new HttpSessionSecurityContextRepository();
-  }
-
-  /**
-   * ユーザー認証に使用するユーザー情報を提供するサービスを構成する。 メモリ内でユーザー名 "user" とパスワード "password" を管理する。
-   *
-   * @return ユーザー情報を提供するサービス
-   */
-  @Bean
-  public UserDetailsService userDetailsService() {
-    // シンプルなユーザー情報をメモリ内で管理するInMemoryUserDetailsManagerを使用
-    // パスワードエンコーダーを使用して、"user"というユーザー名と"password"というパスワードを設定
-    // 修正後は、user、passwordを使用しないとログインできない。
-    UserDetails userDetails = User.builder()
-        .username("user")
-        .password("password")
-        .roles("USER")
-        .build();
-
-    // InMemoryUserDetailsManagerにユーザー情報を設定して返す
-    return new InMemoryUserDetailsManager(userDetails);
   }
 
   /**
