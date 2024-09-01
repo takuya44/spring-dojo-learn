9bf0efb18ca998369516e486c0dd225a4f479b07
takuya44 <shinntaku2345@gmail.com>
2024-09-01 22:35:23 +0900

    add: トップページへのアクセスをテスト（XSRF）

--- src/test/java/com/example/blog/it/RegistrationAndLoginIT.java
+++ src/test/java/com/example/blog/it/RegistrationAndLoginIT.java
@@ -2,6 +2,7 @@ package com.example.blog.it;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.Optional;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -15,6 +16,24 @@ public class RegistrationAndLoginIT {
 
   @Test
   public void integrationTest() {
-    assertThat(webTestClient).isNotNull();
+    // ## Arrange ##
+
+    // ## Act ##
+    var responseSpec = webTestClient.get().uri("/").exchange();
+
+    // ## Action ##
+    var response = responseSpec.returnResult(String.class);
+    var xsrfTokenOpt = Optional.ofNullable(response.getResponseCookies().getFirst("XSRF-TOKEN"));
+
+    // レスポンスのステータスコードが 204 No Content であることを検証
+    responseSpec.expectStatus().isNoContent();
+
+    // Cookie ヘッダーにXSRF-TOKENがある+空文字かどうかをチェック。
+    assertThat(xsrfTokenOpt)
+        .isPresent() // Optional<null>かOptional<xsrfTokenCookie = "aaa">の検証
+        .hasValueSatisfying(xsrfTokenCookie ->
+                assertThat(xsrfTokenCookie.getValue()).isNotBlank()
+            // Optional<xsrfTokenCookie = "aaa">の値が空文字かどうかチェック。
+        );
   }
 }
