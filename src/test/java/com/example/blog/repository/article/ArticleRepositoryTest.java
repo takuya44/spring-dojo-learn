03d5f4ef083fa059fd225a991b04106fb30ac86b
takuya44 <shinntaku2345@gmail.com>
2024-09-09 23:30:20 +0900

    fix: @MyBatisTest はデフォルトで in-memory database に接続する問題を修正

--- src/test/java/com/example/blog/repository/article/ArticleRepositoryTest.java
+++ src/test/java/com/example/blog/repository/article/ArticleRepositoryTest.java
@@ -5,8 +5,10 @@ import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.Test;
 import org.mybatis.spring.boot.test.autoconfigure.MybatisTest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 
 @MybatisTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class ArticleRepositoryTest {
 
   @Autowired
