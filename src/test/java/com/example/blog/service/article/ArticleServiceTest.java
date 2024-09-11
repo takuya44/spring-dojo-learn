ebb0fa14fdc2e20e420bada2280569989152333d
takuya44 <shinntaku2345@gmail.com>
2024-09-12 00:11:07 +0900

    feat: Serivce のテスト（モックなし）：findByIdがOptional<ArticleEntity> を返す場合

--- src/test/java/com/example/blog/service/article/ArticleServiceTest.java
+++ src/test/java/com/example/blog/service/article/ArticleServiceTest.java
@@ -3,9 +3,11 @@ package com.example.blog.service.article;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.jdbc.Sql;
 import org.springframework.transaction.annotation.Transactional;
 
 /**
@@ -31,5 +33,44 @@ class ArticleServiceTest {
   @Test
   public void cut() {
     assertThat(cut).isNotNull();
+  }
+
+  /**
+   * 指定されたIDの記事が存在する場合に、{@link ArticleService#findById(int)} が正しく {@link ArticleEntity}
+   * を返すことを検証するテストメソッド。
+   *
+   * <p>テストの準備段階で、@Sqlアノテーションを使用して、テストデータとしてID 999の記事をデータベースに挿入しています。</p>
+   *
+   * <p>テストの主な検証項目:
+   * <ul>
+   *   <li>ID 999の記事が正しくデータベースから取得されること。</li>
+   *   <li>取得された記事の各フィールド（ID、タイトル、本文、作成日、更新日）が期待通りの値であること。</li>
+   * </ul>
+   * </p>
+   */
+  @Test
+  @DisplayName("findById: 指定されたIDの記事が存在するとき、ArticleEntityを返す")
+  @Sql(statements = {"""
+      INSERT INTO articles (id, title, body, created_at, updated_at)
+      VALUES (999, 'title_999', 'body_999', '2010-10-01 00:00:00', '2010-11-01 00:00:00');
+      """
+  })
+  public void findById_returnArticleEntity() {
+    // ## Arrange ##
+
+    // ## Act ##
+    var actual = cut.findById(999);
+
+    // ## Assert ##
+    assertThat(actual)
+        .isPresent()// Optional が空でないことを確認
+        // 各フィールドの値が期待通りかどうかを確認
+        .hasValueSatisfying(article -> {
+          assertThat(article.id()).isEqualTo(999);
+          assertThat(article.title()).isEqualTo("title_999");
+          assertThat(article.content()).isEqualTo("body_999");
+          assertThat(article.createdAt()).isEqualTo("2010-10-01T00:00:00");
+          assertThat(article.updatedAt()).isEqualTo("2010-11-01T00:00:00");
+        });
   }
 }
