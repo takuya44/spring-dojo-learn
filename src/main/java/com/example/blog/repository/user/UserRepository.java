49dd39959893d41ba38a264b7a88a73fe7f8b411
takuya44 <shinntaku2345@gmail.com>
2024-11-15 23:59:57 +0900

    fix :selectByUsername に null が渡されたとき SQL が実行されないよう修正

--- src/main/java/com/example/blog/repository/user/UserRepository.java
+++ src/main/java/com/example/blog/repository/user/UserRepository.java
@@ -11,6 +11,21 @@ import org.apache.ibatis.annotations.Select;
 @Mapper
 public interface UserRepository {
 
+  /**
+   * 指定された username に基づいてユーザー情報を取得します。
+   * <p>
+   * このデフォルトメソッドは、入力された username が null の場合に SQL クエリを実行せずに空の {@link Optional} を返します。 username が非
+   * null の場合は、{@link #selectByUsernameInternal(String)} メソッドを使用して 実際のデータベースクエリを実行します。
+   * </p>
+   *
+   * @param username 検索対象のユーザー名（null の場合はクエリを実行しません）
+   * @return ユーザー情報が存在する場合は {@link Optional<UserEntity>} を返し、 存在しない場合は空の {@link Optional} を返します。
+   */
+  default Optional<UserEntity> selectByUsername(String username) {
+    return Optional.ofNullable(username) // → Optiona<string> = "test_user"
+        .flatMap(this::selectByUsernameInternal); // ネストを防止 → Optiona<Optional<UserEntity>> = {{}};
+  }
+
   @Select("""
       SELECT
           id
@@ -20,7 +35,7 @@ public interface UserRepository {
       FROM users u
       WHERE u.username = #{username}
       """)
-  Optional<UserEntity> selectByUsername(@Param("username") String username);
+  Optional<UserEntity> selectByUsernameInternal(@Param("username") String username);
 
   @Insert("""
       INSERT INTO users(username, password, enabled)
