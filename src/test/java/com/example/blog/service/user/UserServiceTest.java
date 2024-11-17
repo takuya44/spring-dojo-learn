522820597368fa56f87956135adfdee0e491ec50
takuya44 <shinntaku2345@gmail.com>
2024-11-17 13:19:41 +0900

    feat :UserService のテスト

---
+++ src/test/java/com/example/blog/service/user/UserServiceTest.java
@@ -0,0 +1,51 @@
+package com.example.blog.service.user;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.example.blog.repository.user.UserRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
+@Transactional
+class UserServiceTest {
+
+  @Autowired
+  private UserService cut;
+
+  @Autowired
+  private UserRepository userRepository;
+
+  @Test
+  void successAutowired() {
+    assertThat(cut).isNotNull();
+  }
+
+  @Test
+  @DisplayName("register: ユーザーがデータベースに登録される")
+  void register_success() {
+    // ## Arrange ##
+    var username = "test_username";
+    var password = "test_password";
+
+    // ## Act ##
+    cut.register(username, password);
+
+    // ## Assert ##
+    // passwordとenabled以外の値は、UserRepositoryのテストで検証済みのため、省略。
+    var actual = userRepository.selectByUsername(username);
+    
+    assertThat(actual).hasValueSatisfying(actualEntity -> {
+      assertThat(actualEntity.getPassword())
+          .describedAs("入力された生のパスワードがハッシュ化されていること")
+          .isNotEmpty()
+          .isNotEqualTo(password);
+      assertThat(actualEntity.isEnabled())
+          .describedAs("ユーザー新規登録時には、 有効なアカウントとして登録する")
+          .isTrue();
+    });
+  }
+}
