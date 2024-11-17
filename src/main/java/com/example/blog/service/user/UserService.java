c268656519433a07688bb21d50917871025e71f0
takuya44 <shinntaku2345@gmail.com>
2024-11-17 12:14:48 +0900

    HotFix :insert の引数を UserEntity に変更

--- src/main/java/com/example/blog/service/user/UserService.java
+++ src/main/java/com/example/blog/service/user/UserService.java
@@ -16,7 +16,8 @@ public class UserService {
   @Transactional
   public void register(String username, String rawPassword) {
     var encodedPassword = passwordEncoder.encode(rawPassword);
-    userRepository.insert(username, encodedPassword, true);
+    var newUser = new UserEntity(null, username, encodedPassword, true);
+    userRepository.insert(newUser);
   }
 
   @Transactional
