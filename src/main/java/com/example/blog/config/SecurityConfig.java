03c71f489708f04e7c717e2bb5a379867cea7a80
takuya44 <shinntaku2345@gmail.com>
2024-11-23 23:38:45 +0900

    feat :Bean重複エラー

--- src/main/java/com/example/blog/config/SecurityConfig.java
+++ src/main/java/com/example/blog/config/SecurityConfig.java
@@ -13,7 +13,6 @@ import org.springframework.security.authentication.dao.DaoAuthenticationProvider
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -101,14 +100,14 @@ public class SecurityConfig {
     return new HttpSessionSecurityContextRepository();
   }
 
-  /**
-   * パスワードエンコーダーを構成する。 NoOpPasswordEncoder を使用し、パスワードのエンコードは行わない。 （セキュリティ上の理由から、実運用では推奨されない）
-   *
-   * @return パスワードエンコーダー
-   */
-  @Bean
-  public PasswordEncoder passwordEncoder() {
-    return new BCryptPasswordEncoder();
-  }
+//  /** 削除理由：SecurityConfigとPassEncoderConfig両方をimportすると同じ＠Beanが2つ以上定義されエラーになるため
+//   * パスワードエンコーダーを構成する。 NoOpPasswordEncoder を使用し、パスワードのエンコードは行わない。 （セキュリティ上の理由から、実運用では推奨されない）
+//   *
+//   * @return パスワードエンコーダー
+//   */
+//  @Bean
+//  public PasswordEncoder passwordEncoder() {
+//    return new BCryptPasswordEncoder();
+//  }
 }
 
