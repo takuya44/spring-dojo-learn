7e1521402971e45c770360a9127ba211f77e205f
takuya44 <shinntaku2345@gmail.com>
2024-11-30 18:32:50 +0900

    feat :ハリテーションエラー時のレスポンスホティを実装

--- src/main/java/com/example/blog/web/controller/user/UserRestController.java
+++ src/main/java/com/example/blog/web/controller/user/UserRestController.java
@@ -1,6 +1,7 @@
 package com.example.blog.web.controller.user;
 
 import com.example.blog.api.UsersApi;
+import com.example.blog.model.BadRequest;
 import com.example.blog.model.UserDTO;
 import com.example.blog.model.UserForm;
 import com.example.blog.service.user.UserService;
@@ -8,6 +9,8 @@ import java.security.Principal;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.DataBinder;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.RestController;
@@ -113,5 +116,16 @@ public class UserRestController implements UsersApi {
     return ResponseEntity
         .created(location) // LocationヘッダーにユーザーのURLを設定
         .body(dto);        // 作成したユーザー情報をレスポンスに含む
+  }
+
+  @ExceptionHandler(MethodArgumentNotValidException.class)
+  public ResponseEntity<BadRequest> handleMethodArgumentNotValidException(
+      MethodArgumentNotValidException e
+  ) {
+    var body = new BadRequest();
+    body.setTitle(e.getBody().getTitle());
+
+    return ResponseEntity
+        .badRequest().body(body);
   }
 }
