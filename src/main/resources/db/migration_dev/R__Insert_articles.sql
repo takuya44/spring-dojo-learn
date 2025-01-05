a88469da4696b88874b26dcdba783ba5e14b1bbe
takuya44 <shinntaku2345@gmail.com>
2025-01-05 11:34:57 +0900

    fix :articles：データベース変更_articlesとusers を紐づける

--- src/main/resources/db/migration_dev/R__Insert_articles.sql
+++ src/main/resources/db/migration_dev/R__Insert_articles.sql
@@ -1,13 +1,3 @@
-DELETE FROM articles;
-
-ALTER TABLE articles AUTO_INCREMENT = 1;
-
-INSERT INTO articles (title, body)
-VALUES ('タイトルです1', '1本文です。')
-     , ('タイトルです2', '2本文です。')
-     , ('タイトルです3', '3本文です。')
-;
-
 DELETE FROM users;
 
 ALTER TABLE users AUTO_INCREMENT = 1;
@@ -18,3 +8,14 @@ VALUES (1, 'user1', '$2a$10$B0SVL8nAzks3ES5G8cFS1ulpS4OZLQyCvnjzi0ISWx9FrXVzKbkL
      , (2, 'user2', '$2a$10$JbGAmUhXF.KFaI3VrnL8ZOxzVNCOnMqonPlL82uHF7O2qwROSLUWK', true)
      , (3, 'user3', '$2a$10$/x0LDWs0tk6V90.VQ2ObdezEgBCDKuoLi1LWtIe49NKUrc7LqrWGq', true)
 ;
+
+DELETE FROM articles;
+
+ALTER TABLE articles AUTO_INCREMENT = 1;
+
+INSERT INTO articles (title, body, user_id)
+VALUES ('タイトルです1', '1本文です。', 1)
+     , ('タイトルです2', '2本文です。', 1)
+     , ('タイトルです3', '3本文です。', 2)
+;
+
