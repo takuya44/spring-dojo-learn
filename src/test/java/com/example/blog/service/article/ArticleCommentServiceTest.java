e7c3f45e029e59fcd23e02144a362a141441ce3d
takuya44 <shinntaku2345@gmail.com>
2025-03-27 19:18:19 +0900

    feat :GET comments_200 OK：ArticleCommentService-createのテスト

--- src/test/java/com/example/blog/service/article/ArticleCommentServiceTest.java
+++ src/test/java/com/example/blog/service/article/ArticleCommentServiceTest.java
@@ -1,11 +1,13 @@
 package com.example.blog.service.article;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.when;
 
 import com.example.blog.config.MybatisDefaultDatasourceTest;
 import com.example.blog.config.PasswordEncoderConfig;
 import com.example.blog.service.DateTimeService;
+import com.example.blog.service.exception.ResourceNotFoundException;
 import com.example.blog.service.user.UserService;
 import com.example.blog.util.TestDateTimeUtil;
 import org.junit.jupiter.api.DisplayName;
@@ -103,5 +105,47 @@ class ArticleCommentServiceTest {
         .isEqualTo(commentAuthor);
 
     assertThat(actual.getCreatedAt()).isEqualTo(expectedCurrentDateTime);
+  }
+
+  /**
+   * 指定された記事が存在しない場合に、createメソッドが ResourceNotFoundException をスローすることを検証するテスト。
+   *
+   * <p>
+   * 【テストの流れ】
+   * <ol>
+   *   <li><b>Arrange:</b>
+   *     <ul>
+   *       <li>モックされた DateTimeService を利用して、現在日時を固定値 (2020/01/02 10:20) に設定。</li>
+   *       <li>ユーザーサービスを通じて、コメント作成者を登録する。</li>
+   *       <li>無効な記事ID (0) を指定し、存在しない記事をシミュレートする。</li>
+   *       <li>テスト用のコメント内容を設定する。</li>
+   *     </ul>
+   *   </li>
+   *   <li><b>Act & Assert:</b>
+   *     <ul>
+   *       <li>無効な記事IDを渡して cut.create を実行し、ResourceNotFoundException がスローされることを assertThrows で検証する。</li>
+   *     </ul>
+   *   </li>
+   * </ol>
+   * </p>
+   */
+  @Test
+  @DisplayName("create: 指定された記事が存在しないとき ResourceNotFoundException を投げる")
+  void create_articleDoesNotExist() {
+    // ## Arrange ##
+    // 固定された日時をモックで設定
+    var expectedCurrentDateTime = TestDateTimeUtil.of(2020, 1, 2, 10, 20);
+    when(mockDateTimeService.now()).thenReturn(expectedCurrentDateTime);
+
+    // コメント作成者を登録
+    var commentAuthor = userService.register("test_username2", "test_password");
+    // テスト用のコメント内容と、存在しない記事IDを設定
+    var expectedComment = "コメントしました";
+    var invalidArticleId = 0;
+
+    // Act & Assert: 無効な記事IDの場合、ResourceNotFoundException が発生することを検証
+    assertThrows(ResourceNotFoundException.class, () -> {
+      cut.create(commentAuthor.getId(), invalidArticleId, expectedComment);
+    });
   }
 }
